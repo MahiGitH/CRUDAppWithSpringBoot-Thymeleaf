65cc2b6f0f862d3579cbec70ada97f304ac57c39
MahiGitH <mahiadamu1@gmail.com>
2022-06-06 21:51:26 -0400

    first commit

---
+++ src/main/java/SpringBoot/controller/EmployeeController.java
@@ -0,0 +1,81 @@
+package SpringBoot.controller;
+
+import SpringBoot.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import SpringBoot.service.EmployeeService;
+
+import java.util.List;
+
+@Controller
+public class EmployeeController {
+
+    // Display list of employees
+
+    @Autowired
+    private EmployeeService employeeService;
+    @GetMapping("/")
+    public String viewHomePage(Model model) {
+       return findPaginated(1,"firstName", "asc", model);
+    }
+
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model){
+        // create model attribute to bind form data
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
+    }
+
+    @PostMapping("/SaveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        // save employee to database
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable(value = "id")long id, Model model){
+        // get employee form the service
+        Employee employee = employeeService.getEmployeeById(id);
+        // set employee as per attribute to pre-populate the form
+        model.addAttribute("employee", employee);
+        return "update_employee";
+
+    }
+
+    @GetMapping("/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable(value = "id") long id){
+        // call delete method from the service
+        this.employeeService.deleteEmployeeById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo,
+                                @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir,
+                                Model model) {
+        int pageSize = 5;
+
+        Page < Employee > page = employeeService.findPaginated(pageNo, pageSize, sortField, sortDir);
+        List < Employee > listEmployees = page.getContent();
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
+        model.addAttribute("listEmployees", listEmployees);
+        return "index";
+
+    }
+}
+
